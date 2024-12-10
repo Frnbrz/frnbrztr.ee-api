c27da943841e91395cc9bdbf40ae5eea14115e51
frnbrz <frnbrz@gmail.com>
2024-12-10 23:28:20 +0100

    feat: add database connection and versioning to API routes

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import express, { type Express } from 'express'
 import helmet from 'helmet'
 import { pino } from 'pino'
 
+import { connectDB } from './config/db'
 import { router } from './router'
 
 const logger = pino({ name: 'server start' })
 const app: Express = express()
-
+connectDB()
 // Set the application to trust the reverse proxy
 app.set('trust proxy', true)
 
@@ -17,5 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 app.use(helmet())
 
-app.use(router)
+// Add version to the API
+app.use('/api/v1', router)
+
 export { app, logger }
